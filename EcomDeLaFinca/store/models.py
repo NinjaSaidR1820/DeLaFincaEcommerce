2e8f16ec5d3e1f98f34fbcb5d9ccefc390c52138
NinjaSaidR1820 <saidrivas1820@hotmail.com>
2025-08-29 23:16:40 -0600

    Create Models, Urls and Admin View

--- EcomDeLaFinca/store/models.py
+++ EcomDeLaFinca/store/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+# Categories of Products
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+
+# Customers
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+# Our Products
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+    created_at = models.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.name
+
+# Customer Orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    address = models.CharField(max_length=200, default='', blank=True)
+    phone = models.CharField(max_length=20, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer} - {self.product} ({self.quantity})"
