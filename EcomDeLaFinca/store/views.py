b8f005d332232b72400edd20d27f4d61512540ed
NinjaSaidR1820 <saidrivas1820@hotmail.com>
2025-09-04 01:32:35 -0600

    Add user authentication with login and logout functionality

--- EcomDeLaFinca/store/views.py
+++ EcomDeLaFinca/store/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import redirect, render
 from .models import Product, Category, Order, Customer  
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 
 def home(request):
     products = Product.objects.all()
@@ -8,3 +13,26 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful!")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid UserName or Password. Please try again.")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+    
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home')
